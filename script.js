a8f1ee046fae387eacfc8d116274d7e23343d4c5
Hisyam <hisyamibadurrahman7@gmail.com>
2026-06-28 09:50:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function showMessage(){
+
+document.getElementById("message").innerHTML=`
+
+<h2>❤️</h2>
+
+<p>
+
+Aku berharap di umurmu yang ke-18,
+kamu selalu bahagia.
+
+Terima kasih telah menjadi bagian
+dari hidupku.
+
+Happy Birthday.
+
+</p>
+
+`;
+
+}
